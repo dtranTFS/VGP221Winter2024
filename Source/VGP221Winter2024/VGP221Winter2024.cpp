9ec07a37bc3cdcbe7d0c0a23f9ed55f52b850a6d
Danny Tran <dtran@torontofilmschool.ca>
2024-01-10 20:07:40 -0500

    Initial Project
    Summary:
    - Created an empty UE5 C++ Project

---
+++ Source/VGP221Winter2024/VGP221Winter2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VGP221Winter2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VGP221Winter2024, "VGP221Winter2024" );
